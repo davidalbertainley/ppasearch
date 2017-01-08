e3b3ea07f50bca22bf7088b03f91eaea367c6101
David Ainley <davidalbertainley@gmail.com>
2017-01-08 18:55:58 -0500

    If no results & counter.
    Tells the user how many results were found and lets them know if no results were found.

--- ppasearch_py.py
+++ ppasearch_py.py
@@ -99,17 +99,31 @@ def next_page_check(soup):
 def spit_out_results():
 	# Create the counter for PPAs so that we can have a nice number to select.
 	ppa_counter = 0
+	
+	# Create the counter to make sure that there are results.
+	ppa_counter_2 = 0
+	
+	for ppa_result in totally_clean_list_of_names:
+		ppa_counter_2 += 1
+	
+	if ppa_counter_2 == 0:
+		print("Sorry, no results were found.")
+	elif ppa_counter_2 > 0:
+		ppa_counter_2 = str(ppa_counter_2)
+		print("Good news!  Found " + ppa_counter_2 + " results!")
+		print("\n")
+		
 
 	# For each PPA in ppa_result, print it and then figure out how many spaces to add to it.
 	for ppa_result in totally_clean_list_of_names:
 		ppa_counter += 1
 		ppa_counter_string = str(ppa_counter)
 		if ppa_counter < 10:
-			print(ppa_counter_string + "     " + ppa_result)
+			print("[" + ppa_counter_string + "]" + "     " + ppa_result)
 		elif ppa_counter < 100:
-			print(ppa_counter_string + "    " + ppa_result)
+			print("[" + ppa_counter_string + "]" + "    " + ppa_result)
 		elif ppa_counter < 1000:
-			print(ppa_counter_string + "   " + ppa_result)
+			print("[" + ppa_counter_string + "]" + "   " + ppa_result)
 
 
 
